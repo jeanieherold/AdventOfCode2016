be1c1722643d660a9294510bc0d9a4d88e05bb87
jeanieherold <jeaniemherold68@gmail.com>
2017-05-29 18:03:27 -0400

    day5p2 begin

---
+++ Day5/Day5part2/myscripts.js
@@ -0,0 +1,47 @@
+// Advent of Code 2016 Day 5 Part 2 - find password from hashed door id- in a better hash
+//variables
+var prefix;
+var index = 0;
+var stringToHash;
+var hexHash = '';
+var password = new Array(8);
+
+function find5ZeroHash (prefix, index) {
+	
+	for (var i = 0; i < count; i++) {
+		while((!(hexHash[0] === '0' && hexHash[1] === '0' && hexHash[2] === '0' && hexHash[3] === '0' && hexHash[4] === '0') && (count < 8))  {
+			stringToHash = prefix + index;
+			hexHash = SparkMD5.hash(stringToHash);
+			index++;
+		}
+		if ([hexHash[6]] < password.length) {
+			if (typeof password[hexHash[6] === 'undefined']) {
+				password[(hexHash[5])] = hexHash[6];
+				count++;
+			}
+		}
+		console.log(password);
+		console.log(stringToHash);
+		console.log(hexHash);
+		hexHash = '';
+	}
+	return password;
+}
+
+//main section --> 
+//read input file
+document.getElementById("openFile").addEventListener('change', function() {
+	var fr = new FileReader();
+	fr.onload = function() { 
+		//results of file read
+		read = this.result;
+		
+		//do stuff with the file:
+
+		console.log(find5ZeroHash (read, index));
+
+		//display input file to the page
+		document.getElementById('fileContents').textContent = read;
+	}
+	fr.readAsText(this.files[0]);
+})
